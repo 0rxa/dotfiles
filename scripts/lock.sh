78fc076c7499c2ad1cd8cfc796060c3a666b9bc7
DarkUlixes <reiarifi@gmail.com>
2020-03-13 14:10:54 +0100

    changed path

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #i3lock -c '#000000' -o '#191d0f' -w '#572020' -l '#ff7f00' -e
 #i3lock -c '#000000' -o '#191d0f' -w '#013c57' -l '#e0ebf1' -e
-/home/rxa/Apps/Source/betterlockscreen/betterlockscreen -l blur && modprobe -r psmouse && modprobe psmouse
+/home/rxa/.scripts/betterlockscreen -l blur && modprobe -r psmouse && modprobe psmouse
