e73ecb8e555b63f2af12561b5d471cae68915587
DarkUlixes <reiarifi@gmail.com>
2020-03-24 13:58:09 +0100

    script to change monitors easily

---
+++ scripts/monitors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+prefix=$HOME/.scripts/monitors/
+profile=$(find $HOME/.scripts/monitors -type f \
+	| awk -F '/' '{print $NF}' \
+	| awk -F '.' '{print $1}' \
+	| dmenu -i)
+bash $prefix$profile.sh
+i3-msg restart
