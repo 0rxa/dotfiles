ae1001f6553bb66aed9aa6fe7c4488c088489117
DarkUlixes <reiarifi@gmail.com>
2020-03-26 21:19:49 +0100

    exit if no input

--- scripts/monitors.sh
+++ scripts/monitors.sh
@@ -5,5 +5,6 @@ profile=$(find $HOME/.scripts/monitors -type f \
 	| awk -F '/' '{print $NF}' \
 	| awk -F '.' '{print $1}' \
 	| dmenu -i)
+[[ $profile = "" ]] && exit
 bash $prefix$profile.sh
 i3-msg restart
